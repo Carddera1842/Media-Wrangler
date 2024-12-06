dc4a11fb62347107f175eae39f85d6982c334793
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-12-06 12:04:13 -0600

    upload back end

---
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/MediaWranglerApplication.java
@@ -0,0 +1,13 @@
+package com.mediawrangler.media_wrangler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediaWranglerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediaWranglerApplication.class, args);
+	}
+
+}
