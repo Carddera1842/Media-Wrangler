288ae1601a2634fd0ec753126e480738825cb14d
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-12-12 20:30:52 -0600

    changing because it belongs on different branch

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/data/JournalEntryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mediawrangler.media_wrangler.data;
-
-import com.mediawrangler.media_wrangler.models.JournalEntry;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface JournalEntryRepository extends CrudRepository<JournalEntry, Integer> {
-}
