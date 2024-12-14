3af96b112e54313d36d4e8a5d0a0fc759606bf9f
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-12-14 10:57:02 -0600

    centered nav

--- media-wrangler-front-end/src/components/home-page/Nav.jsx
+++ media-wrangler-front-end/src/components/home-page/Nav.jsx
@@ -1,27 +1,21 @@
 import * as React from 'react';
+import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
-import Box from '@mui/material/Box';
 
-export default function ColorTabs() {
-  const [value, setValue] = React.useState('one');
+export default function CenteredTabs() {
+  const [value, setValue] = React.useState(0);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
   };
 
   return (
-    <Box sx={{ width: '100%' }}>
-      <Tabs
-        value={value}
-        onChange={handleChange}
-        textColor="secondary"
-        indicatorColor="secondary"
-        aria-label="secondary tabs example"
-      >
-        <Tab value="one" label="Item One" />
-        <Tab value="two" label="Item Two" />
-        <Tab value="three" label="Item Three" />
+    <Box sx={{ width: '100%', bgcolor: 'background.paper' }}>
+      <Tabs value={value} onChange={handleChange} centered>
+        <Tab label="Home" />
+        <Tab label="Item Two" />
+        <Tab label="Item Three" />
       </Tabs>
     </Box>
   );
