dc144fab83a9da5dc7ac0bec5f20ac5e5aef4df0
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-12-20 12:36:29 -0600

    active tab state persists on page reload

--- media-wrangler-front-end/src/components/nav/Navbar.jsx
+++ media-wrangler-front-end/src/components/nav/Navbar.jsx
@@ -1,11 +1,21 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 import Box from '@mui/material/Box';
 import Tabs from '@mui/material/Tabs';
 import Tab from '@mui/material/Tab';
 
 export default function Navbar() {
+  const location = useLocation();
   const [value, setValue] = React.useState(0);
+
+  const tabRoutes = ['/', '/movies', '/search', '/login', '/register'];
+
+  React.useEffect(() => {
+    const currentTab = tabRoutes.indexOf(location.pathname);
+    if (currentTab !== -1) {
+      setValue(currentTab);
+    }
+  }, [location.pathname]);
 
   const handleChange = (event, newValue) => {
     setValue(newValue);
