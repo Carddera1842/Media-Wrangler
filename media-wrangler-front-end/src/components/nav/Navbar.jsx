57288084f913dac46d72d51074874c3207b92de4
mpres08 <158608642+mpres08@users.noreply.github.com>
2024-12-20 12:30:59 -0600

    fixed sizing bug

--- media-wrangler-front-end/src/components/nav/Navbar.jsx
+++ media-wrangler-front-end/src/components/nav/Navbar.jsx
@@ -13,21 +13,43 @@ export default function Navbar() {
 
   return (
     <Box sx={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        bgcolor: 'background.paper',
-        padding: 2
-      }}>
-        <Box sx={{ fontSize: 36, fontWeight: 'bold', fontFamily: 'sans-serif' }}>
+      display: 'flex',
+      flexDirection: { xs: 'comumn', sm: 'row' },
+      alignItems: 'center',
+      justifyContent: 'center',
+      bgcolor: 'background.paper',
+      padding: 2,
+      // flexWrap: 'wrap',
+      // flexShrink: 0,
+    }}>
+      <Box sx={{ 
+        fontSize: { xs: 24, sm: 36 }, 
+        fontWeight: 'bold', 
+        fontFamily: 'sans-serif', 
+        textAlign: { sx: 'center', sm: 'left'},
+        flexShrink: 0,
+        }}>
           Media Wrangler
         </Box>
         <Box sx={{
-          width: '50%',
+          flexGrow: 1,
+          maxWidth: '100%',
+          minWidth: 0,
           bgcolor: 'Background.paper',
           padding: 2
         }}>
-        <Tabs value={value} onChange={handleChange} centered>
+        <Tabs 
+          value={value} 
+          onChange={handleChange} 
+          centered
+          variant="scrollable"
+          scrollButtons="auto"
+          // sx={{
+          //   '& .MuiTabs-flexContainer': {
+          //     justifyContent: { xs: 'center', sm: 'space-between' },
+          //   },
+          // }}
+        >
           <Tab label="Home" component={Link} to="/" sx={{ marginX: 3 }} />
           <Tab label="Movies" component={Link} to="/movies" sx={{ marginX: 3 }} />
           <Tab label="Search" component={Link} to="/search" sx={{ marginX: 3 }} />
