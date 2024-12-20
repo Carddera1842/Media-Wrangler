d6279d4eeb675cd2d6be0b09e0b7d9491057b463
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2024-12-20 00:10:11 -0600

    Ready to add API fetch

--- media-wrangler-front-end/src/components/MovieCard.jsx
+++ media-wrangler-front-end/src/components/MovieCard.jsx
@@ -1,21 +1,58 @@
 import React from 'react';
-import movie from '../assets/Its-a-wonderful-lfe.jpg';
+
 
 function MovieCard() {
 
+    //The API fetching should go here I believe
+    //update properties in the return statement to match the API json 
+    //maybe an if/else statement should be used to display an error message in case search comes up empty or something??
+    
+
+
+    //Testing the map() function
+    const movies = [
+        {id: 1, 
+            title: "It's a Wonderful Life",
+            poster: "https://image.tmdb.org/t/p/original/qRitcyVpWdL7bSV7akDcKTR2YxL.jpg"   
+        },
+        {id: 2, 
+            title: "Elf", 
+            poster: "https://th.bing.com/th/id/R.4a6b29fcf1ab7cf3691f3bf7fcd2643e?rik=LWBs8HixZr5cUw&riu=http%3a%2f%2fwww.nerdspan.com%2fwp-content%2fuploads%2f2013%2f12%2felf-movie-poster.jpg&ehk=wNRWgnn0Hmn8ACJ8cWPtVRUO0bHZuqR4Bbqf9qaDPbs%3d&risl=&pid=ImgRaw&r=0" 
+        },
+        {id: 3, 
+            title: "National Lampoon's Christmas Vacation",
+            poster: "https://th.bing.com/th/id/R.125ac48e9aa9e14ae04cef81ec106f33?rik=TtGyrHK5QucY7A&riu=http%3a%2f%2fstatic1.squarespace.com%2fstatic%2f57488e28746fb940f103c64e%2f574f321fb09f95f3b30449b5%2f5bd0f04124a69482aaa57017%2f1545267192036%2fChristmas%2bVacation%2bPoster.jpg%3fformat%3d1500w&ehk=SAHawDiZJirI61qbNe1xvB%2f3Zoccx2tDprTPyAdwbrY%3d&risl=&pid=ImgRaw&r=0"
+        }
+    ]
+
+
+
     return(
         <div>
-            <img src={movie} alt="It's a Wonderful Life" width="200" height="300"></img>
-            <p>It's a Wonderful Life (1946)</p>
+            {movies.map((movie) => (
+            <div key={movie.id}>
+            <img
+            src={movie.poster} 
+            alt={movie.title} 
+            style={{ width: '200px', height: '300px' }} 
+            />
+            <h3>{movie.title}</h3> 
         </div>
+            ))}
+        </div>
+
+     
     );
 }
 
 export default MovieCard;
 
 /*
-Currently I have placeholder values, to make it dynamic we will use a loop that goes over the array of movie objects that is returned from the API
-<img src={moviePosterKey}></img>   <--- The width and height can be adjusted. We probably want a smaller poster size for the poster grid that will display the movie-card and possibly to attach to the movie-review-form
+NOTE: The width and height can be adjusted. We probably want a smaller poster size for the poster grid that will display the movie-cards and possibly to attach to the movie-review-form
 We probably want an image a bit larger for the movie-detail-card when we are targeting in on one specific movie 
-<p>{movieTitleKey}></p>
+
+
+NOTE: We may not want to display the movie title in the poster grid (or horizontal display) because the titles will not allow for a good flow with the various lengths. 
+
+NOTE: We just need to decide if we want the results to list horizontal or vertically. For the homepage, if we are viewing the trending movies I think horizontally would be best, but for the search results a vertical display might be nice. No preference either way
 */
