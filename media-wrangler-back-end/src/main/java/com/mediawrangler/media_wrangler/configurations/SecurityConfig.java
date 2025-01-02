0fd7ea0d77d6feb8bf89cd7aa6ea992ce243244b
Carddera1842 <carddera1842@gmail.com>
2025-01-02 11:29:01 -0600

    Streamlining

---
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/configurations/SecurityConfig.java
@@ -0,0 +1,23 @@
+package com.mediawrangler.media_wrangler.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig implements WebMvcConfigurer {
+
+
+}
+
+
