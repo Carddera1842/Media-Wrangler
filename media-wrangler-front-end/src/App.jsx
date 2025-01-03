cac90f84d0238fa7e635e4de1f02d66895da01ce
mpres08 <158608642+mpres08@users.noreply.github.com>
2025-01-02 20:33:36 -0600

    eliminated registration success

--- media-wrangler-front-end/src/App.jsx
+++ media-wrangler-front-end/src/App.jsx
@@ -5,8 +5,10 @@ import Movies from './components/Movies/Movies'
 import Search from './components/Search/Search'
 import Home from './components/Home/Home'
 import Login from './components/Login/Login'
+import LoginSuccess from './components/Login/LoginSuccess'
 import Register from './components/Register/Register'
 import registrationSuccess from './components/Register/RegistrationSuccess'
+import ProfileHeader from './components/Profile/ProfileHeader'
 import Profile from './components/Profile/Profile'
 
 function App() {
@@ -20,9 +22,9 @@ function App() {
         <Route path="/movies" element={<Movies />}/>
         <Route path="/search" element={<Search />}/>
         <Route path="/login" element={<Login />}/>
-        {/* <Route path="/loginsuccess" element={<LoginSuccess />}/> */}
+        <Route path="/loginsuccess" element={<LoginSuccess />}/>
         <Route path="/register" element={<Register />}/>
-        <Route path="/registrationsuccess" element={<registrationSuccess />}/>
+        {/* <Route path="/registrationsuccess" element={<registrationSuccess />}/> */}
         <Route path="/profile/:userId" element={<Profile />}/>
       </Routes>
         </div>
