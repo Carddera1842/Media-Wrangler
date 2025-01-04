b04842bc0267dbc9c19c9e7ec1ae41a95c690027
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-04 02:16:00 -0600

    Add code that got deleted off other branch, mouseOver and mouseOut events

--- media-wrangler-front-end/src/components/MovieCard.jsx
+++ media-wrangler-front-end/src/components/MovieCard.jsx
@@ -1,16 +1,8 @@
 import React, { useState } from 'react';
 import './MovieCard.css';
-
-
 function MovieCard() {
-
-    //The API fetching should go here I believe
-    //update properties in the return statement to match the API json 
-    //maybe an if/else statement should be used to display an error message in case search comes up empty or something??
-    
-
-
-    //Testing the map() function
+ 
+    //Testing the map() function -- will use demo data until search filters or user movie lists are available
     const movies = [
         {id: 1, 
             title: "It's a Wonderful Life",
@@ -25,22 +17,17 @@ function MovieCard() {
             poster: "https://th.bing.com/th/id/R.125ac48e9aa9e14ae04cef81ec106f33?rik=TtGyrHK5QucY7A&riu=http%3a%2f%2fstatic1.squarespace.com%2fstatic%2f57488e28746fb940f103c64e%2f574f321fb09f95f3b30449b5%2f5bd0f04124a69482aaa57017%2f1545267192036%2fChristmas%2bVacation%2bPoster.jpg%3fformat%3d1500w&ehk=SAHawDiZJirI61qbNe1xvB%2f3Zoccx2tDprTPyAdwbrY%3d&risl=&pid=ImgRaw&r=0"
         }
     ]
-
-    const [isHovered, setIsHovered] = useState(false);
-
-    function handleMouseOver(){
-      setIsHovered(true);     
-    };
-  
-    function handleMouseOut(){
-      setIsHovered(false);
-    };
- 
-    function handleClick() {
-        console.log("Poster Clicked");
-    }
-
-
+    //This should track which movie id is being hovered over. isHovered state variable was allowing all movies to highlight at once
+    const [hoveredId, setHoveredId] = useState(null); 
+  function handleMouseOver(id) {
+    setHoveredId(id); 
+  }
+  function handleMouseOut() {
+    setHoveredId(null); 
+  }
+  function handleClick() {
+    console.log("Poster Clicked");
+  }
     return(
         <div>
         <div id="movie-search">
@@ -48,22 +35,27 @@ function MovieCard() {
             <div key={movie.id}>
                 <img
                 onClick={handleClick}
-                onMouseOver= {handleMouseOver}
+                onMouseOver= {() => handleMouseOver(movie.id)}
                 onMouseOut={handleMouseOut}
                 src={movie.poster} 
                 alt={movie.title} 
+                style={{
+                    border: hoveredId === movie.id ? '2px solid rgb(99, 180, 176)' : 'none', 
+                    transition: 'border 0.3s ease', 
+                  }}
                 />
                 
+            {/* Picked up the highlight styling from googling it. It can be changed to whatever we want.
+                Also, the poster onClick event does work. Now I'll just need to reroute it to the movie detail view */}
+           
                 
             </div>
             ))}
             </div>
         </div>
-
      
     );
 }
-
 export default MovieCard;
 
 /*
