f255f5341feb0349b0b02fca24732340e76f5784
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-06 17:23:47 -0600

    Add MovieReviewServe and incorporated it into the Post handler in MovieReviewController that accepts the new movie review object

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/MovieReviewController.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/MovieReviewController.java
@@ -3,6 +3,7 @@ package com.mediawrangler.media_wrangler.controllers;
 
 import com.mediawrangler.media_wrangler.data.MovieReviewRepository;
 import com.mediawrangler.media_wrangler.models.MovieReview;
+import com.mediawrangler.media_wrangler.services.MovieReviewService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,15 +16,27 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api/reviews")
 public class MovieReviewController {
 
+    //Based off other branches, setup Service here
+    private final MovieReviewService movieReviewService;
+
+    //Constructor injection of MovieReviewService -- constructor that takes in service
+    public MovieReviewController(MovieReviewService movieReviewService) {
+        this.movieReviewService = movieReviewService;
+    }
+
     //Add movieReviewRepository to perform CRUD functions
     @Autowired
     private MovieReviewRepository movieReviewRepository;
 
+
+
+    //I currently do not have validation or errors to be checked for tester form
+    // Changed the saveReview object in the return response to a String message stating success
     @PostMapping("/create")
         public ResponseEntity<?> createReview(@RequestBody MovieReview movieReview) {
         try {
-            MovieReview savedReview = movieReviewRepository.save(movieReview);
-            return new ResponseEntity<>(savedReview, HttpStatus.CREATED);
+            MovieReview savedReview = movieReviewService.saveReview(movieReview);
+            return new ResponseEntity<>("Review Submission successful", HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>("An error occurred while saving the review", HttpStatus.INTERNAL_SERVER_ERROR);
         }
