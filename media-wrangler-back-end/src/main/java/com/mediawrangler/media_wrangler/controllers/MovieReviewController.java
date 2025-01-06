e18ae363856d1d1d865f8a5392ecda9030b9f36b
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-06 14:05:57 -0600

    Add more debugging in application.properties & made a list of things I have tried at the bottom of MovieReviewController

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/MovieReviewController.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/MovieReviewController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 //Add CrossOrigin annotation to allow HTTP request/response exchange between front and back end
-@CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+@CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
 @RestController
 @RequestMapping("/api/reviews")
 public class MovieReviewController {
@@ -29,4 +29,44 @@ public class MovieReviewController {
         }
     }
 
+
+
 }
+
+
+/*
+    I have tried ...
+    -- @CrossOrigin(origins = "http://localhost:5173")
+    -- @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
+    -- @CrossOrigin(origins = "http://localhost:5173", allowCredentials = "true")
+    -- removing it completely since there is WebConfig available
+    -- cleared browser cache
+    -- tried to put a handler to deal with OPTIONS request specifically:
+        @RequestMapping(value = "/create", method = RequestMethod.OPTIONS)
+        public ResponseEntity<?> handleOptions() {
+         return ResponseEntity.ok().build();  // Respond to OPTIONS request (preflight)
+        }
+    -- tried to add "Origin" and credentials on the frontend fetch in MovieReviewForm.jsx
+            fetch("http://localhost:8080/api/users/create", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Origin": "http://localhost:5173",
+            },
+            body: JSON.stringify({ review }),
+            credentials: "include"
+        });
+    -- Added some code in application.properties to give me more info in terminal console of what is happening:
+            logging.level.org.springframework.web=DEBUG
+            logging.level.org.springframework.security=DEBUG
+
+
+
+        *** I do not understand how else to get the Access-Control-Allow-Origin header in my response***
+
+
+
+
+
+ */
+
