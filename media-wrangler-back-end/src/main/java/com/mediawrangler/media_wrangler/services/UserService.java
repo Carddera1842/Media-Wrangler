3fc2e262e1153d85f001f1d6a6895c25d83804b4
Carddera1842 <carddera1842@gmail.com>
2025-01-08 20:25:46 -0600

    Added dependencies

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/services/UserService.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/services/UserService.java
@@ -2,28 +2,47 @@ package com.mediawrangler.media_wrangler.services;
 
 import com.mediawrangler.media_wrangler.models.User;
 import com.mediawrangler.media_wrangler.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JavaMailSender mailSender;
 
     public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User saveUser(User user) {
+    public void saveUser(User user, String siteURL) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
-        return userRepository.save(user);
+
+        String randomCode = RandomString.make(64);
+        user.setVerificationCode(randomCode);
+        user.setEnabled(false);
+
+        userRepository.save(user);
+
+        sendVerificationEmail(user, siteURL);
     }
 
     public boolean authenticate(String username, String password) {
         User user = userRepository.findByUsername(username);
         return user != null && passwordEncoder.matches(password, user.getPassword());
+    }
+
+    private void sendVerificationEmail(User user, String siteURL) {
+
     }
 }
 
