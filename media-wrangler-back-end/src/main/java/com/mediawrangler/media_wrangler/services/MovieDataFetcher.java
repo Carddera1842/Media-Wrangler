c205e7ac8c5807516efe4377eef39f66d686c0bc
HenryLuepke <henryluepke4@gmail.com>
2025-01-16 11:38:16 -0600

    added person search method to moviedatafetcher

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/services/MovieDataFetcher.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/services/MovieDataFetcher.java
@@ -90,6 +90,92 @@ public class MovieDataFetcher {
         return movieArrayList;
     }
 
+    // returns array of movies where searched person was involved
+    public static ArrayList<Movie> personSearch(String searchString) {
+        OkHttpClient client = new OkHttpClient();
+        ArrayList<Movie> movieArrayList = new ArrayList<>();
+        int personId = 0;
+
+        // fetches person id
+        String idFetchUrl = "https://api.themoviedb.org/3/search/person?query=" + searchString + "&include_adult=false&language=en-US&page=1";
+        Request idRequest = new Request.Builder()
+                .url(idFetchUrl)
+                .header("Authorization", "Bearer " + API_READ_ACCESS_KEY)
+                .build();
+
+        try (Response response = client.newCall(idRequest).execute()) {
+            if (!response.isSuccessful()) {
+                System.out.println("Search request failed with status: " + response.code());
+            }
+
+            // parse search results
+            String responseBody = response.body().string();
+            JSONObject jsonResponse = new JSONObject(responseBody);
+
+            // check if the result contains movies
+            JSONArray results = jsonResponse.getJSONArray("results");
+            if (results.length() > 0) {
+                // Return the ID of the first movie found
+                personId = results.getJSONObject(0).getInt("id");
+            } else {
+                System.out.println("No cast found with that name.");
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        for (int i = 1; i < 6; i++) {
+            String movieFetchUrl = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page="
+                    + i
+                    + "&sort_by=popularity.desc&with_people="
+                    + personId;
+
+            Request movieRequest = new Request.Builder()
+                    .url(movieFetchUrl)
+                    .header("Authorization", "Bearer " + API_READ_ACCESS_KEY)
+                    .build();
+
+            try (Response response = client.newCall(movieRequest).execute()) {
+                if (!response.isSuccessful()) {
+                    System.out.println("Request failed with status: " + response.code());
+                    return null;
+                }
+
+                // parse response and transform into array
+                String responseBody = response.body().string();
+                JSONObject jsonResponse = new JSONObject(responseBody);
+                JSONArray results = jsonResponse.getJSONArray("results");
+
+                // extract details for each movie
+                for (int j = 0; j < results.length(); j++) {
+                    JSONObject movieJson = results.getJSONObject(j);
+
+                    // Extract the required fields for the Movie object
+                    int id = movieJson.getInt("id");
+                    String title = movieJson.getString("title");
+                    String releaseDate = movieJson.getString("release_date");
+                    double rating = movieJson.getDouble("vote_average");
+                    String overview = movieJson.getString("overview");
+                    String posterPath = movieJson.optString("poster_path", null);
+
+
+                    Movie movie = new Movie(id, title, releaseDate, rating, overview, posterPath);
+
+                    movieArrayList.add(movie);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
+
+        return movieArrayList;
+    }
+
     // searches for a movie ID by title
     private static int searchMovieIdByTitle(OkHttpClient client, String title) {
         String apiUrl = "https://api.themoviedb.org/3/search/movie?" + "query=" + title + "&language=en-US";
