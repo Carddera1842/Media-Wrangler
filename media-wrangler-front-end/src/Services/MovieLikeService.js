4317157fdafd2391e1bb8dd3bfc6c19b0159fa7c
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-21 21:29:57 -0600

    issues with 403 error when submitting like to the backend

---
+++ media-wrangler-front-end/src/Services/MovieLikeService.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+    async function submitMovieLike(data) {
+        try {
+            const response = await axios.post(
+                "http://localhost:8080/api/likes/movies", {
+                   data , 
+                    withCredentials: true,
+                }
+            );
+            console.log("Response:", response);
+            if (response.status === 201) {          
+                console.log("Like saved for movie");
+                return "Success"
+            } else {
+                return ("Movie Like failed. Please try again");
+            }
+        } catch (error) {
+            return ("An error occurred. Please try again", error);
+        }
+    }
+
+
+    async function removeMovieLike(movieId, userId) {
+        try {
+            const response = await axios.delete(
+                'http://localhost:8080/like/movies', {
+                    data: { movieId, userId },  
+                    withCredentials: true,
+                }
+            );
+            console.log("Response:", response);
+            if (response.status === 200) { 
+                console.log("Like removed for movie");
+                return "Success"
+            } else {
+                return ("Removing the like failed. Please try again");
+            }
+        } catch (error) {
+            console.error("An error occurred:", error);
+            return ("An error occurred. Please try again");
+        }
+    }
+
+    export { submitMovieLike, removeMovieLike };
