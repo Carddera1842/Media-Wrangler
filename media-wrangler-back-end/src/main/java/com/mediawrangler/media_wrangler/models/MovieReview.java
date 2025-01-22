e852c730f8b8e135482bfab8096e7d71490a6735
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-22 12:30:12 -0600

    small fix on dateCreated for review and comment

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/models/MovieReview.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/models/MovieReview.java
@@ -6,6 +6,7 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,9 +19,12 @@ public class MovieReview {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    //stamps date for when the review is submitted/created
     private LocalDate dateCreated;
 
+    @PrePersist
+    protected void onCreate() {
+        this.dateCreated = LocalDate.now();
+    }
 
     @NotNull(message = "You must enter a date watched")
     private LocalDate dateWatched;
