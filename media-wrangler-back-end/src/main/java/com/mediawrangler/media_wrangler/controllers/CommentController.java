60d8d6c2bc28be14e1dc4a24f45a8ecc1d077a24
Rachel Burkhardt <r.burkhardt.ibclc@gmail.com>
2025-01-23 12:34:47 -0600

    Working on backend for comments

--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/CommentController.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/controllers/CommentController.java
@@ -2,8 +2,14 @@ package com.mediawrangler.media_wrangler.controllers;
 
 
 import com.mediawrangler.media_wrangler.data.CommentRepository;
+import com.mediawrangler.media_wrangler.dto.CommentDTO;
+import com.mediawrangler.media_wrangler.dto.MovieReviewDTO;
 import com.mediawrangler.media_wrangler.models.Comment;
 import com.mediawrangler.media_wrangler.services.CommentService;
+
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "http://localhost:5173", allowedHeaders = "*", allowCredentials = "true")
 @RestController
+@RequestMapping("/comments")
 public class CommentController {
 
 
@@ -28,14 +35,22 @@ public class CommentController {
     }
 
     @PostMapping("/movies")
-    public ResponseEntity<?> createComment(@RequestBody Comment comment) {
+    public ResponseEntity<?> saveComment(@RequestBody Comment comment) {
         try {
-            Comment savedComment = commentService.saveComment(comment); // Use the service instance
+            Comment savedComment = commentService.saveComment(comment); 
             return new ResponseEntity<>("Comment Submission successful", HttpStatus.CREATED);
         } catch (Exception e) {
             return new ResponseEntity<>("An error occurred while saving the comment", HttpStatus.INTERNAL_SERVER_ERROR);
         }
     }
+
+
+    @GetMapping("/movie-review/{movieReviewId}")
+    public ResponseEntity<List<CommentDTO>> getCommentsByMovieReviewId(@PathVariable Long movieReviewId) {
+        List<CommentDTO> comments = commentService.findCommentsByMovieReviewId(movieReviewId);
+        return ResponseEntity.ok(comments);
+    }
+
 
 
 }
--- media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/dto/CommentDTO.java
+++ media-wrangler-back-end/src/main/java/com/mediawrangler/media_wrangler/dto/CommentDTO.java
@@ -6,18 +6,30 @@ public class CommentDTO {
 
 
     private Long id;
-
     private String userComment;
-
     private LocalDate dateCreated;
-
-
+    private Long movieReviewId; 
+    private int userId; 
+    private String userName; 
+    
     public CommentDTO() {
     }
 
-    public CommentDTO(String userComment) {
+    public CommentDTO(Long id, String userComment, LocalDate dateCreated, Long movieReviewId, int userId, String userName) {
+        this.id = id;
         this.userComment = userComment;
-        this.dateCreated = LocalDate.now();
+        this.dateCreated = dateCreated;
+        this.movieReviewId = movieReviewId;
+        this.userId = userId;
+        this.userName = userName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getUserComment() {
@@ -28,5 +40,36 @@ public class CommentDTO {
         this.userComment = userComment;
     }
 
+    public LocalDate getDateCreated() {
+        return dateCreated;
+    }
+
+    public void setDateCreated(LocalDate dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public Long getMovieReviewId() {
+        return movieReviewId;
+    }
+
+    public void setMovieReviewId(Long movieReviewId) {
+        this.movieReviewId = movieReviewId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
 
 }
