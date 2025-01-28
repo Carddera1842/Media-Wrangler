952cab9a1bf0dfab74f42c43e694a9d14b9677b0
Carddera1842 <carddera1842@gmail.com>
2025-01-28 13:43:45 -0600

    small change

--- media-wrangler-front-end/src/components/Login/Login.jsx
+++ media-wrangler-front-end/src/components/Login/Login.jsx
@@ -64,7 +64,7 @@ export default function Login() {
 
                         <div className="login-field">
                         <div className="login-button-container">
-                        <button className="login-button is-primary">Register</button>
+                        <button className="login-button is-primary">Login</button>
                             </div>
                         </div>
                     </Form>
